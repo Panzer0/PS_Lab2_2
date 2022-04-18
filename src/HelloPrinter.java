1c6d7465706d804df2eac4dd65ab5c8ae60f96f8
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-04-19 00:47:34 +0200

    Initial commit

---
+++ src/HelloPrinter.java
@@ -0,0 +1,17 @@
+public class HelloPrinter implements Runnable {
+
+    public void run() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String args[]) {
+        try {
+            Thread helloThread = new Thread(new HelloPrinter());
+            helloThread.start();
+            helloThread.join();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
